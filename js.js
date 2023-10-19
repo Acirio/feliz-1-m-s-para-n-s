c89debdc1575e50520f9e5a92efeec53ab5b517b
Acirio <89374059+Acirio@users.noreply.github.com>
2023-10-19 17:46:08 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,195 @@
+/* Aula 20 MaiaQuiz  */
+
+let titulo = document.querySelector('h1')
+let instrucoes = document.querySelector('#instrucoes')
+let aviso = document.querySelector('#aviso')
+//let respostaEsta = document.querySelector('#respostaEsta')
+let pontos = 0 // pontos para o placar
+let placar = 0 // placar
+
+// PERGUNTA
+let numQuestao = document.querySelector('#numQuestao')
+let pergunta   = document.querySelector('#pergunta')
+
+// ALTERNATIVAS
+let a = document.querySelector('#a')
+let b = document.querySelector('#b')
+let c = document.querySelector('#c')
+
+// article com a class questoes
+let articleQuestoes = document.querySelector('.questoes')
+// ol li com as alternativas
+let alternativas = document.querySelector('#alternativas')
+
+const q0 = {
+    numQuestao   : 0,
+    pergunta     : "Pergunta",
+    alternativaA : "Alternativa A",
+    alternativaB : "Alternativa B",
+    alternativaC : "Alternativa C",
+    correta      : "0",
+}
+
+const q1 = {
+    numQuestao   : 1,
+    pergunta     : "Qual foi o dia do nosso primeiro beijo?",
+    alternativaA : "No niver de Analet",
+    alternativaB : "No dia dos teodolitos",
+    alternativaC : "Isso nunca aconteceu",
+    correta      : "No niver de Analet",
+}
+
+const q2 = {
+    numQuestao   : 2,
+    pergunta     : "Qual foi o dia que eu te pedi em namnoro?",
+    alternativaA : "Dia 20 de maio",
+    alternativaB : "Dia 20 de junho",
+    alternativaC : "Dia 20 de julho",
+    correta      : "Dia 20 de julho",
+}
+
+const q3 = {
+    numQuestao   : 3,
+    pergunta     : "Quantos meses de namoro completamos hoje?",
+    alternativaA : "2 meses",
+    alternativaB : "3 meses",
+    alternativaC : "4 meses",
+    correta      : "3 meses",
+}
+
+const q4 = {
+    numQuestao   : 4,
+    pergunta     : "O que eu mais amo nesse mundo?",
+    alternativaA : "Ficar com você",
+    alternativaB : "Te beijar todinha",
+    alternativaC : "Todas as duas alternativas",
+    correta      : "Todas as duas alternativas",
+}
+
+const q5 = {
+    numQuestao   : 5,
+    pergunta     : "O que mais quero nesse mundo agora",
+    alternativaA : "Ir pro show do Matuê",
+    alternativaB : "Ver o ABC vencer uma partida",
+    alternativaC : "Te ver de novo pra dar um abraço bem grande e te encher de beijos",
+    correta      : "Te ver de novo pra dar um abraço bem grande e te encher de beijos",
+}
+
+// CONSTANTE COM UM ARRAY DE OBJETOS COM TODAS AS QUESTOES
+const questoes = [q0, q1, q2, q3, q4, q5]
+
+let numero = document.querySelector('#numero')
+let total  = document.querySelector('#total')
+
+numero.textContent = q1.numQuestao
+
+let totalDeQuestoes = (questoes.length)-1
+console.log("Total de questões " + totalDeQuestoes)
+total.textContent = totalDeQuestoes
+
+// MONTAR A 1a QUESTAO COMPLETA, para iniciar o Quiz
+numQuestao.textContent = q1.numQuestao
+pergunta.textContent   = q1.pergunta
+a.textContent = q1.alternativaA
+b.textContent = q1.alternativaB
+c.textContent = q1.alternativaC
+
+// CONFIGURAR O VALUE INICIAL DA 1a QUESTAO COMPLETA
+a.setAttribute('value', '1A')
+b.setAttribute('value', '1B')
+c.setAttribute('value', '1C')
+
+// PARA MONTAR AS PROXIMAS QUESTOES
+function proximaQuestao(nQuestao) {
+    numero.textContent = nQuestao
+    numQuestao.textContent = questoes[nQuestao].numQuestao
+    pergunta.textContent   = questoes[nQuestao].pergunta
+    a.textContent = questoes[nQuestao].alternativaA
+    b.textContent = questoes[nQuestao].alternativaB
+    c.textContent = questoes[nQuestao].alternativaC
+    a.setAttribute('value', nQuestao+'A')
+    b.setAttribute('value', nQuestao+'B')
+    c.setAttribute('value', nQuestao+'C')
+}
+
+function bloquearAlternativas() {
+    a.classList.add('bloqueado')
+    b.classList.add('bloqueado')
+    c.classList.add('bloqueado')
+}
+
+function desbloquearAlternativas() {
+    a.classList.remove('bloqueado')
+    b.classList.remove('bloqueado')
+    c.classList.remove('bloqueado')
+}
+
+function verificarSeAcertou(nQuestao, resposta) {
+
+    let numeroDaQuestao = nQuestao.value
+    console.log("Questão " + numeroDaQuestao)
+
+    let respostaEscolhida = resposta.textContent
+    //console.log("RespU " + respostaEscolhida)
+
+    let certa = questoes[numeroDaQuestao].correta
+    //console.log("RespC " + certa)
+
+    if(respostaEscolhida == certa) {
+        //console.log("Acertou")
+        //respostaEsta.textContent = "Correta 😊"
+        pontos += 10 // pontos = pontos + 10
+    } else {
+        //console.log("Errou!")
+        //respostaEsta.textContent = "Errada 😢"
+    }
+
+    // atualizar placar
+    placar = pontos
+    instrucoes.textContent = "Pontos " + placar
+
+    // bloquear a escolha de opcoes
+    bloquearAlternativas()
+
+    setTimeout(function() {
+        //respostaEsta.textContent = '...'
+        proxima = numeroDaQuestao+1
+
+        if(proxima > totalDeQuestoes) {
+            console.log('Fim do Jogo!')
+            fimDoJogo()
+        } else {
+            proximaQuestao(proxima)
+        }
+    }, 250)
+    desbloquearAlternativas()
+}
+
+function fimDoJogo() {
+    instrucoes.textContent = "Fim de Jogo!"
+    numQuestao.textContent = ""
+
+    let pont = ''
+    pontos == 0 ? pont = 'ponto' : pont = 'pontos'
+
+    pergunta.textContent   = "Você conseguiu " + pontos + " " + pont
+
+    aviso.textContent = "Você conseguiu " + pontos + " " + pont
+
+    a.textContent = ""
+    b.textContent = ""
+    c.textContent = ""
+
+    a.setAttribute('value', '0')
+    b.setAttribute('value', '0')
+    c.setAttribute('value', '0')
+
+    // OCULTAR O ARTICLE DA QUESTAO
+    articleQuestoes.style.display = 'none'
+
+   //
+    setTimeout(function() {
+        pontos = 0 // zerar placar
+        window.open('https://wa.me/5584997021311?text=Oi+amor+da+minha+vida%2C+pessoa+que+mais+amo+nesse+mundo%2C+j%C3%A1+terminei+o+quiz%21');
+    }, 2000)
+}
